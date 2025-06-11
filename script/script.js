07ea8e20d541c0933197c2c385a6f7e74add36a0
PoiseAdeSetiawan <206706144+PoiseAdeSetiawan@users.noreply.github.com>
2025-06-11 10:18:40 +0700

    fix service button target

--- script/script.js
+++ script/script.js
@@ -140,10 +140,3 @@ document.addEventListener('DOMContentLoaded', function () {
         toggleNavigationMenu();
     });
 });
-
-document.querySelectorAll('.service_button').forEach(btn => {
-    btn.addEventListener('click', function () {
-        const href = btn.getAttribute('href');
-        if (href) window.open(href, '_blank');
-    });
-});
